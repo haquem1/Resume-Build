86c15a781505c3cbd1cf1ed15c8525e9fb70a150
haquem1 <maroof.haque@gmail.com>
2017-06-14 15:46:30 -0700

    made progress towards fixing UI consistency bugs

--- resume-build/public/app.js
+++ resume-build/public/app.js
@@ -1,38 +1,34 @@
 $(function(){
-                $('#download-pdf').click(function(e){
-                    e.preventDefault();
-                    console.log('select_link clicked');
+    $('#download-pdf').click(function(e){
+        e.preventDefault();
+        console.log('select_link clicked');
 
-                     /*$.ajax({
-                        dataType: 'jsonp',
-                        data: "data=yeah",
-                        jsonp: 'callback',
-                        url: 'http://localhost:3000/endpoint?callback=?',
-                        success: function(data) {
-                            console.log('success');
-                            console.log(JSON.stringify(data));
-                        }
-                    });*/
-					var data = {};
-					data.title = "title";
-					data.message = "message";
+			  var data = {
+          name: "fullname",
+          email: "email",
+          phone: "phone",
+          school: "school",
+          major: "major",
+          course: "course",
+          awards: "award",
+          experience: {
+            job: "job title",
+            company: "company",
+            dateWorked: "dates worked",
+            desc: "desc"
+          }
+        };
 
-					$.ajax({
+
+				$.ajax({
 						type: 'POST',
 						data: JSON.stringify(data),
 				        contentType: 'application/json',
-                        url: 'http://localhost:3000/endpoint',
+                        url: '/makePDF',
                         success: function(data) {
                             console.log('success');
                             console.log(JSON.stringify(data));
                         }
                     });
-					/*$.ajax('http://localhost:3000/endpoint', {
-					        type: 'POST',
-					        data: JSON.stringify(data),
-					        contentType: 'application/json',
-					        success: function() { console.log('success');},
-					        error  : function() { console.log('error');}
-					});*/
                 });
             });
--- resume-build/routes/index.js
+++ resume-build/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var pug = require('pug');
+var fs = require('fs');
 var data = require('../models/data.json');
+
+var PDFDocument = require('pdfkit');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,12 +14,40 @@ router.get('/', function(req, res, next) {
 
 /* GET created resume and render HTML string to PDF */
 router.get('/pdf', function(req, res, next) {
+  // Compile a function
+  var doc = new PDFDocument();
+  var filename = req.body.filename;
+  // Stripping special characters
+  // Stripping special characters
+  filename = encodeURIComponent(filename) + '.pdf';
+  // Setting response to 'attachment' (download).
+  // If you use 'inline' here it will automatically open the PDF
+  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
+  res.setHeader('Content-type', 'application/pdf');
+  var content = req.body.content;
+  doc.y = 300;
+  doc.text(content, 50, 50);
+  doc.pipe(res);
+  doc.end();
+  fs.readFile('./views/index.pug', 'utf8', function (err, file) {
+    if (err) throw err;
+    console.log(file);
+    var fn = pug.compileFile(file);
+    // var html = fn({data: data});
+    // console.log(html);
+    //
+    // pdf.create(html).toFile('./resume.pdf', function(err, res) {
+    //   if (err) return console.log(err);
+    //   console.log(res); // { filename: '/app/businesscard.pdf' }
+    // });
+  });
+
   res.render('index', { data : data });
 });
 
 /* POST created resume and render HTML string to PDF */
 router.post('/makePDF', function(req, res, next) {
-  res.render('index', { data : req.data });
+  res.render('index', { data : req.body });
 });
 
 module.exports = router;
