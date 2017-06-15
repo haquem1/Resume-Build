db5ca9c6512a2679aa99ef496c7c8406914780c5
haquem1 <maroof.haque@gmail.com>
2017-06-14 19:17:57 -0700

    successfully streaming generated pdfs

--- resume-build/public/app.js
+++ resume-build/public/app.js
@@ -1,34 +1,34 @@
-$(function(){
-    $('#download-pdf').click(function(e){
-        e.preventDefault();
-        console.log('select_link clicked');
-
-			  var data = {
-          name: "fullname",
-          email: "email",
-          phone: "phone",
-          school: "school",
-          major: "major",
-          course: "course",
-          awards: "award",
-          experience: {
-            job: "job title",
-            company: "company",
-            dateWorked: "dates worked",
-            desc: "desc"
-          }
-        };
-
-
-				$.ajax({
-						type: 'POST',
-						data: JSON.stringify(data),
-				        contentType: 'application/json',
-                        url: '/makePDF',
-                        success: function(data) {
-                            console.log('success');
-                            console.log(JSON.stringify(data));
-                        }
-                    });
-                });
-            });
+// $(function(){
+//     $('#download-pdf').click(function(e){
+//         e.preventDefault();
+//         console.log('select_link clicked');
+//
+// 			  var data = {
+//           name: "fullname",
+//           email: "email",
+//           phone: "phone",
+//           school: "school",
+//           major: "major",
+//           course: "course",
+//           awards: "award",
+//           experience: {
+//             job: "job title",
+//             company: "company",
+//             dateWorked: "dates worked",
+//             desc: "desc"
+//           }
+//         };
+//
+//
+// 				$.ajax({
+// 						type: 'POST',
+// 						data: JSON.stringify(data),
+// 				        contentType: 'application/json',
+//                         url: '/makePDF',
+//                         success: function(data) {
+//                             console.log('success');
+//                             console.log(JSON.stringify(data));
+//                         }
+//                     });
+//                 });
+//             });
--- resume-build/routes/index.js
+++ resume-build/routes/index.js
@@ -6,6 +6,10 @@ var data = require('../models/data.json');
 
 var PDFDocument = require('pdfkit');
 
+var pdf = require('html-pdf');
+var fn = pug.compileFile('./views/index.pug', {filename: "index"});
+var html = fn({data: data});
+var options = { format: 'Letter' };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,40 +18,18 @@ router.get('/', function(req, res, next) {
 
 /* GET created resume and render HTML string to PDF */
 router.get('/pdf', function(req, res, next) {
-  // Compile a function
-  var doc = new PDFDocument();
-  var filename = req.body.filename;
-  // Stripping special characters
-  // Stripping special characters
-  filename = encodeURIComponent(filename) + '.pdf';
-  // Setting response to 'attachment' (download).
-  // If you use 'inline' here it will automatically open the PDF
-  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
-  res.setHeader('Content-type', 'application/pdf');
-  var content = req.body.content;
-  doc.y = 300;
-  doc.text(content, 50, 50);
-  doc.pipe(res);
-  doc.end();
-  fs.readFile('./views/index.pug', 'utf8', function (err, file) {
-    if (err) throw err;
-    console.log(file);
-    var fn = pug.compileFile(file);
-    // var html = fn({data: data});
-    // console.log(html);
-    //
-    // pdf.create(html).toFile('./resume.pdf', function(err, res) {
-    //   if (err) return console.log(err);
-    //   console.log(res); // { filename: '/app/businesscard.pdf' }
-    // });
+  pdf.create(html).toStream(function(err, stream){
+    if (err) return console.log(err);
+    stream.pipe(res);
   });
-
-  res.render('index', { data : data });
 });
 
 /* POST created resume and render HTML string to PDF */
 router.post('/makePDF', function(req, res, next) {
-  res.render('index', { data : req.body });
+  pdf.create(html).toStream(function(err, stream){
+    if (err) return console.log(err);
+    stream.pipe(res);
+  });
 });
 
 module.exports = router;
