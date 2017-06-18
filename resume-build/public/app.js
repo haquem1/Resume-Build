150140a0896d754ec3656e4198b3a850f12c04b7
haquem1 <maroof.haque@gmail.com>
2017-06-17 23:34:45 -0700

    packaging client json object for POST

--- resume-build/public/app.js
+++ resume-build/public/app.js
@@ -1,8 +1,9 @@
 $(function(){
     $('#download-pdf').click(function(e){
-        //ajax post here
+      e.preventDefault(); // for testing
+        // ajax post here
         var data = createData();
-        // on ajax success, do window.location for get
+        // on ajax success
         $.ajax({
           type: 'POST',
           data: JSON.stringify(data),
@@ -12,29 +13,56 @@ $(function(){
                           console.log(JSON.stringify(data));
                       }
         });
+        console.log(data);
         return true;
     });
 });
 
 function createData () {
+
+  var length, i, j, k = 0;
+  var education = {};
+  var experience = {};
+  var skills = {};
+
   // for loop to create json "array" for courses awards skills and jobs
+  for (k = 0; k < 3; k++) {
+    if (k === 0) {
+      for (i = 1; i < $('.education article').length; i++) {
+        education[i] = {};
+        education[i].school =  $('.education').children(".school:eq("+ i +")").text();
+        education[i].major = $('.education').children(".major:eq("+ i +")").text();
+        education[i].awards = {};
+        education[i].coursework = {};
+        for (j = 1; j < $('#activity-award-list li').length; j++) {
+          education[i].coursework[j] = $('#activity-award-list').children("label:eq("+ j +")").text();
+        }
+        for (j = 1; j < $('#course-list li').length; j++) {
+          education[i].awards[j] = $('#course-list').children("label:eq("+ j +")").text();
+        }
+      }
+    } else if (k === 1) {
+        for (i = 1; i < $('.experience article').length; i++) {
+          experience[i] = {};
+          experience[i].job =  $('.experience').children(".job:eq("+ i +")").text();
+          experience[i].company =  $('.experience').children(".company:eq("+ i +")").text();
+          experience[i].dateWorked =  $('.experience').children(".dates:eq("+ i +")").text();
+          experience[i].desc =  $('.experience').children(".desc:eq("+ i +")").text();
+        }
+    } else {
+      for (i = 1; i < $('.skill li').length; i++) {
+        skills[i] =  $('.skill').children("label:eq("+ j +")").text();
+      }
+    }
+  }
+
   var data = {
     "name": $(".name").text(),
-    "email": $(".email").text(),
-    "phone": "phone",
-    "school": "school",
-    "major": "major",
-    "course": "course",
-    "awards": "award",
-    "coursework": {
-      "course":"course"
-    },
-    "experience": {
-      "job": "job title",
-      "company": "company",
-      "dateWorked": "dates worked",
-      "desc": "desc"
-    }
+    "contact": $(".contact").text(),
+    "education": education,
+    "experience": experience,
+    "skills": skills
   };
+
   return data;
 }
--- resume-build/routes/index.js
+++ resume-build/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+/* GET test HTML page to be converted to PDF. */
+router.get('/test', function(req, res, next) {
+  var data = require('../tmp/data.json');
+  res.render('index', {data: data});
+});
+
 module.exports = router;
